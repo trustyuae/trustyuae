4da26bffa24e7a8c96cb7b153b35cf6031c3ba68
seoexpertwt <seoexpertwt@gmail.com>
2024-05-01 10:33:37 +0530

    designed order management system

--- src/components/OrderManagementSystem.js
+++ src/components/OrderManagementSystem.js
@@ -1,71 +1,60 @@
 import React, { useState, useEffect } from 'react';
-import { MDBCheckbox, MDBCard, MDBCardBody, MDBBadge, MDBBtn, MDBTable, MDBTableHead, MDBTableBody } from 'mdb-react-ui-kit';
-import { DataGrid } from '@mui/x-data-grid';
+import axios from 'axios';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import Button from 'react-bootstrap/Button';
+import Table from 'react-bootstrap/Table';
+import Pagination from 'react-bootstrap/Pagination';
 import Form from 'react-bootstrap/Form';
-import axios from 'axios';
-
-const columns = [
-    { field: 'name', headerName: 'Product Name', width: 250 },
-    { field: 'id', headerName: 'ID', width: 70 },
-    { 
-        field: 'image', 
-        headerName: 'Image', 
-        width: 90, 
-        height:30,
-        renderCell: (params) => (
-          <img src={params.value.src} alt="Product" style={{ width: '100%', height: '100%' }} />
-        ),
-      },
-    { field: 'quantity', headerName: 'Quantity', width: 130 },
-    
-];
-
+import Button from 'react-bootstrap/Button';
 
 function OrderManagementSystem() {
-    
     const [orders, setOrders] = useState([]);
-    //pagination
-    const [page, setPage] = useState(1);
-    const [pageSize, setPageSize] = useState(5);
+    const [currentPage, setCurrentPage] = useState(1);
     const [totalPages, setTotalPages] = useState(0);
-
+    const [pageSize, setPageSize] = useState(5); // Number of orders per page
+    const [selectAll, setSelectAll] = useState(false); // Track whether all checkboxes are selected
+    const pageSizes = [5, 10, 15, 20]; // Available page sizes
     const username = 'ck_176cdf1ee0c4ccb0376ffa22baf84c096d5a155a';
     const password = 'cs_8dcdba11377e29282bd2b898d4a517cddd6726fe';
 
     useEffect(() => {
-        const fetchOrders = async () => {
-            try {
-                const response = await axios.get(`https://ghostwhite-guanaco-836757.hostingersite.com/wp-json/wc/v3/orders?status=processing&page=${page}&per_page=${pageSize}`, {
-                    auth: {
-                        username: username,
-                        password: password
-                    }
-                });
-                let a = response.data.map(order=>order.line_items)
-                setOrders([].concat(...a));
-                setTotalPages(response.headers['x-wp-totalpages']);
-            } catch (error) {
-                console.error('Error fetching data:', error);
-            }
-        };
-        fetchOrders();
-    }, [page, pageSize]);
+        fetchOrders(currentPage, pageSize);
+    }, [currentPage, pageSize]);
 
-    const handlePageChange = (newPage) => {
-        setPage(newPage);
+    const fetchOrders = async (page, size) => {
+        try {
+            const response = await axios.get(`https://ghostwhite-guanaco-836757.hostingersite.com/wp-json/wc/v3/orders?status=processing&page=${page}&per_page=${size}`, {
+                auth: {
+                    username: username,
+                    password: password
+                }
+            });
+            console.log(response, 'response');
+            let a = response.data.map(order => order.line_items)
+            console.log([].concat(...a), '[].concat(...a)');
+            setOrders([].concat(...a));
+            setTotalPages(response.headers['x-wp-totalpages']);
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
     };
 
-    const handlePageSizeChange = (newPageSize) => {
-        console.log(newPageSize,'newPageSize');
-        setPageSize(newPageSize);
+    const handlePageChange = (page) => {
+        setCurrentPage(page);
+    };
+
+    const handlePageSizeChange = (e) => {
+        setPageSize(parseInt(e.target.value));
+        setCurrentPage(1); // Reset to first page when page size changes
+    };
+
+    const handleSelectAll = () => {
+        setSelectAll(!selectAll); // Toggle select all state
     };
 
     return (
-        <Container fluid className='py-3' style={{ maxHeight: "100%", minHeight: "100vh" }}>
+        <Container fluid className='p-5' style={{ maxHeight: "100%", minHeight: "100vh" }}>
             <div className="mb-9">
                 <div className="row g-3 mb-4 justify-content-center ">
                     <div className="col-auto">
@@ -81,7 +70,6 @@ function OrderManagementSystem() {
                                 <Form.Label>Date filter:</Form.Label>
                                 <Form.Control
                                     type="date"
-
                                     className="mr-sm-2"
                                 />
                             </Form.Group>
@@ -96,50 +84,80 @@ function OrderManagementSystem() {
                                 />
                             </Form.Group>
                         </Col>
+                        <Col xs="auto" lg="2">
+                            <Form.Group>
+                                <Form.Label>Page Size:</Form.Label>
+                                <Form.Select value={pageSize} onChange={handlePageSizeChange}>
+                                    {pageSizes.map(size => (
+                                        <option key={size} value={size}>{size}</option>
+                                    ))}
+                                </Form.Select>
+                            </Form.Group>
+                        </Col>
                     </Row>
                 </Form>
             </Row>
-            <Row className='mb-4 mt-4'>
-                <MDBCard>
-                    <MDBCardBody>
-                        <div style={{ height: 400, width: '100%' }}>
-                            {/* <DataGrid
-                                rows={orders}
-                                columns={columns}
-                                initialState={{
-                                    pagination: {
-                                        paginationModel: { page: 0, pageSize: 5 },
-                                    },
-                                }}
-                                pageSizeOptions={[5, 10]}
-                                checkboxSelection
-                            /> */}
-                            <DataGrid
-                        rows={orders}
-                        columns={columns}
-                        pagination
-                        pageSize={pageSize}
-                        rowCount={totalPages * pageSize}
-                        paginationMode="server"
-                        onPageChange={handlePageChange}
-                        onPageSizeChange={handlePageSizeChange}
-                    />
-                        </div>
-                    </MDBCardBody>
-                </MDBCard>
+            <Row className='mb-4 mt-4 '>
+                <Table striped bordered hover>
+                    <thead>
+                        <tr>
+                            <th>
+                                <Form.Check
+                                    type="checkbox"
+                                    onChange={handleSelectAll}
+                                    checked={selectAll}
+                                />
+                            </th>
+                            <th>Product Name</th>
+                            <th>Image</th>
+                            <th>Quantity</th>
+                            <th>Order Status</th>
+                            <th>PO Number</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {orders.map(order => (
+                            <tr key={order.id}>
+                                <td>
+                                    <Form.Check
+                                        type="checkbox"
+                                        checked={selectAll}
+                                        onChange={() => {/* handle individual checkbox change if needed */}}
+                                    />
+                                </td>
+                                <td className='text-center'>{order.name}</td>
+                                <td className='text-center'>
+                                    <img src={order.image.src} alt={order.name} className='img-fluid' width={90} />
+                                </td>
+                                <td className='text-center'>{order.quantity}</td>
+                                <td className='text-center'>{order.quantity}</td>
+                                <td className='text-center'>{order.quantity}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </Table>
             </Row>
             <Row className='mb-4 mt-4'>
                 <Form inline>
                     <Row className='mt-4'>
                         <Col xs="auto" lg="3" className=' d-flex  align-items-end'>
-                            <Button type="button" className='mr-2 mx-3 w-50' >Select All Orders</Button>
+                            <Button type="button" className='mr-2 mx-3 w-50' onClick={handleSelectAll}>Select All Orders</Button>
                             <Button type="button" className='mr-2 mx-3 w-50' >Create PO</Button>
                         </Col>
                     </Row>
                 </Form>
             </Row>
+            <Pagination className="mt-3 justify-content-center">
+                <Pagination.Prev onClick={() => handlePageChange(currentPage - 1)} disabled={currentPage === 1} />
+                {[...Array(totalPages).keys()].map(number => (
+                    <Pagination.Item key={number + 1} active={number + 1 === currentPage} onClick={() => handlePageChange(number + 1)}>
+                        {number + 1}
+                    </Pagination.Item>
+                ))}
+                <Pagination.Next onClick={() => handlePageChange(currentPage + 1)} disabled={currentPage === totalPages} />
+            </Pagination>
         </Container>
-    )
+    );
 }
 
-export default OrderManagementSystem
+export default OrderManagementSystem;
