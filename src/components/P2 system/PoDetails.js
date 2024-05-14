90f47b57bbdef098971e1386d6736e5c6fee3a54
seoexpertwt <seoexpertwt@gmail.com>
2024-05-14 18:25:53 +0530

    update api bind

--- src/components/P2 system/PoDetails.js
+++ src/components/P2 system/PoDetails.js
@@ -18,19 +18,22 @@ import axios from 'axios';
 
 const PoDetails = () => {
     const { id } = useParams();
-    const [PO_OrderList,setPO_OrderList]=useState([])
+    const [PO_OrderList, setPO_OrderList] = useState([])
+    const [data, setData] = useState(null);
+    const [paymentStatus,setPaymentStatus]=useState('')
 
-const fetchOrder = async()=>{
-    let apiUrl = `${API_URL}wp-json/custom-po-details/v1/po-order-details/${id}`
-    const response = await axios.get(apiUrl);
-    console.log(response.data,'response');
-    setPO_OrderList(response.data)
-}
+    const fetchOrder = async () => {
+        let apiUrl = `${API_URL}wp-json/custom-po-details/v1/po-order-details/${id}`
+        const response = await axios.get(apiUrl);
+        // console.log(response.data, 'response');
+        setPO_OrderList(response.data)
+        setData(response.data.line_items)
+    }
 
-useEffect(()=>{
-    console.log(id,'id===>>>>>');
-    fetchOrder()
-},[])
+    useEffect(() => {
+        console.log('PO_OrderList:', PO_OrderList);
+        fetchOrder()
+    }, [])
     const tableHeaders = [
         "Product Name",
         "Image",
@@ -99,66 +102,135 @@ useEffect(()=>{
     const totalQty = PODetails.reduce((acc, cur) => acc + cur.qtyOrdered, 0);
     const totalEstdCostRMB = PODetails.reduce((acc, cur) => acc + (cur.qtyOrdered * cur.estimatedCostRMB), 0);
     const totalEstdCostAED = PODetails.reduce((acc, cur) => acc + (cur.qtyOrdered * cur.estimatedCostAED), 0);
-    const availabilityStatus = ['Confirmed','1 week', '2 week', '3 weeks','1 month', 'Out of Stock'];
+    const availabilityStatus = ['Confirmed', '1 week', '2 week', '3 weeks', '1 month', 'Out of Stock'];
+
+    // const handleStatusChange = (index, event) => {
+    //     const newAvailabilityStatus = [...PO_OrderList.availability_status];
+    //     console.log(newAvailabilityStatus,'newAvailabilityStatus');
+    //     // newAvailabilityStatus[index] = event.target.value; // Update the availability status for the corresponding product
+    //     // setPO_OrderList(prevState => ({
+    //     //     ...prevState,
+    //     //     availability_status: newAvailabilityStatus
+    //     // }));
+    // };
+
+    const handleStatusChange = (index, event) => {
+        console.log(index, event);
+        console.log(PO_OrderList.line_items[index].availability_status=event.target.value,'PO_OrderList');
+        console.log(PO_OrderList,'PO_OrderList========');
+        // PO_OrderList?.availability_status[index]
+        // Check if PO_OrderList and availability_status are defined and is an array
+        // if (PO_OrderList && Array.isArray(PO_OrderList.availability_status)) {
+        //     const newAvailabilityStatus = [...PO_OrderList.availability_status];
+        //     newAvailabilityStatus[index] = event.target.value;
+        //     setPO_OrderList(prevState => ({
+        //         ...prevState,
+        //         availability_status: newAvailabilityStatus
+        //     }));
+        // } else {
+        //     console.error("PO_OrderList or availability_status is not defined or not an array");
+        // }
+    };
+
+    const handleUpdate = async() => {
+        console.log(PO_OrderList,'PO_OrderList======');
+        console.log(PO_OrderList.line_items.map(item => item.product_id),'PO_OrderList.line_items.map(item => item.product_id)');
+        const updatedData = {
+          availability_status: PO_OrderList.line_items.map(item => item.availability_status),
+          product_ids: PO_OrderList.line_items.map(item => item.product_id),
+          order_ids: PO_OrderList.line_items.map(item => item.order_id),
+          payment_status: paymentStatus
+        };
+
+        let apiUrl=`${API_URL}wp-json/custom-available-status/v1/estimated-status/${id}`
+        const response = await axios.post(apiUrl,updatedData
+        //     {
+        //   availability_status: PO_OrderList.line_items.map(item => item.availability_status),
+        //   product_ids: PO_OrderList.line_items.map(item => item.product_id),
+        //   order_ids: PO_OrderList.line_items.map(item => item.order_id),
+        //   payment_status: paymentStatus
+        // }
+    );
+        console.log(response,'response');
+        console.log(updatedData,'updatedData');
+    }
+
+    const handlepayMentStatus=(e)=>{
+        setPaymentStatus(e.target.value)
+    }
+
     return (
-        <Container fluid className='px-5'style={{ height:'100vh'}}>
+        <Container fluid className='px-5' style={{ height: '100vh' }}>
             <h3 className='fw-bold text-center my-3'>PO Details</h3>
             <h6 className='fw-bold text-center'>XYZ Guangzhon</h6>
             <p className='text-center my-3'>PO Number: {PO_OrderList.po_id}</p>
             <div className="d-flex justify-content-start align-items-center my-3">
                 <h6 className=''>Payment Status:</h6>
-                <select className="form-select w-25 ms-3" name="" id="">
-                    <option value=''>Paid</option>
-                    <option value=''>Unpaid</option>
-                    <option value=''>Hold</option>
-                    <option value=''>Cancelled</option>
+                <select className="form-select w-25 ms-3" name="" id="" onChange={(e)=>handlepayMentStatus(e)}>
+                    <option value='Paid'>Paid</option>
+                    <option value='Unpaid'>Unpaid</option>
+                    <option value='Hold'>Hold</option>
+                    <option value='Cancelled'>Cancelled</option>
                 </select>
             </div>
             <MDBRow className='d-flex justify-content-center align-items-center'>
                 <MDBCol col='10' md='12' sm='12'></MDBCol>
-               <div style={{overflow:'auto',height:'350px'}}>
-               <Table striped bordered hover style={{boxShadow: '4px 4px 11px 0rem rgb(0 0 0 / 25%)'}}>
-                    <thead>
-                        <tr className='table-headers'>
-                            {tableHeaders.map((header, index) => (
-                                <th style={{ backgroundColor: '#DEE2E6', padding: '8px', textAlign:'center'}} key={index}>{header}</th>
-                            ))}
-                        </tr>
-                    </thead> 
-                    <tbody>
-                        {PO_OrderList?.line_items?.map((rowData, rowIndex) => (
-                            <tr key={rowIndex}>
-                                <td className='text-center'>{rowData.product_name}</td>
-                                <td className='text-center'><img src={rowData.image} style={{width:'100px',height:'100px'}} alt={rowData.product_name} /></td>
-                                <td className='text-center'>{rowData.quantity}</td>
-                                <td className='text-center'>--</td>
-                                <td className='text-center'>--</td>
-                                <td>
-                            <Form.Group>
-                                {/* <Form.Label>Page Size:</Form.Label> */}
-                                <Form.Select  style={{height:'32px',padding:'0 10px'}}>
-                                    {availabilityStatus.map(status => (
-                                        <option key={status} value={status}>{status}</option>
-                                    ))}
-                                </Form.Select>
-                            </Form.Group>
-                            </td>
+                <div style={{ overflow: 'auto', height: '350px' }}>
+                    <Table striped bordered hover style={{ boxShadow: '4px 4px 11px 0rem rgb(0 0 0 / 25%)' }}>
+                        <thead>
+                            <tr className='table-headers'>
+                                {tableHeaders.map((header, index) => (
+                                    <th style={{ backgroundColor: '#DEE2E6', padding: '8px', textAlign: 'center' }} key={index}>{header}</th>
+                                ))}
                             </tr>
-                        ))}
-                    </tbody>
-                    <tfoot>
-                        <tr>
-                            {/* <td colSpan={1}></td> */}
-                            <td colspan="2" className=' text-end'><strong>Total:</strong></td>
-                            <td className='text-center'>{totalQty}</td>
-                            <td className='text-center'>{totalEstdCostRMB}</td>
-                            <td colspan="2">{totalEstdCostAED}</td>
-                        </tr>
-                    </tfoot>
-                </Table>
-               </div>
+                        </thead>
+                        <tbody>
+                            {PO_OrderList?.line_items?.map((rowData, rowIndex) => (
+                                <tr key={rowIndex}>
+                                    <td className='text-center'>{rowData.product_name}</td>
+                                    <td className='text-center'><img src={rowData.image} style={{ width: '100px', height: '100px' }} alt={rowData.product_name} /></td>
+                                    <td className='text-center'>{rowData.quantity}</td>
+                                    <td className='text-center'>--</td>
+                                    <td className='text-center'>--</td>
+                                    <td>
+                                        <Form.Group>
+                                            <Form.Select style={{ height: '32px', padding: '0 10px' }}
+                                                onChange={(event) => handleStatusChange(rowIndex, event)}
+                                                // value={ PO_OrderList?.availability_status[rowIndex] }
+                                            >
+                                                {availabilityStatus.map(status => (
+                                                    <option key={status} value={status}>{status}</option>
+                                                ))}
+                                            </Form.Select>
+                                        </Form.Group>
+                                        {/* <Form.Group>
+                                            <Form.Select
+                                                style={{ height: '32px', padding: '0 10px' }}
+                                                // onChange={(event) => handleStatusChange(rowIndex, event)} // Pass the index of the product and the event object
+                                                value={PO_OrderList.availability_status[rowIndex]} // Set the value of the select dropdown to the current availability status
+                                            >
+                                                {availabilityStatus.map(status => (
+                                                    <option key={status} value={status}>{status}</option>
+                                                ))}
+                                            </Form.Select>
+                                        </Form.Group> */}
+                                    </td>
+                                </tr>
+                            ))}
+                        </tbody>
+                        <tfoot>
+                            <tr>
+                                {/* <td colSpan={1}></td> */}
+                                <td colspan="2" className=' text-end'><strong>Total:</strong></td>
+                                <td className='text-center'>{totalQty}</td>
+                                <td className='text-center'>{totalEstdCostRMB}</td>
+                                <td colspan="2">{totalEstdCostAED}</td>
+                            </tr>
+                        </tfoot>
+                    </Table>
+                </div>
                 <Row>
-                <Button type="button" className='w-auto'>Upload</Button>
+                    <Button type="button" className='w-auto' onClick={handleUpdate}>Update</Button>
                 </Row>
             </MDBRow>
         </Container>
