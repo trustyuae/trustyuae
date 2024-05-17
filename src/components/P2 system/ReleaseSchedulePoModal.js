725d059604191bf11032061f92f85d8375fa2917
codecraftwt <codecraftwt@gmail.com>
2024-05-17 22:55:27 +0530

    applied different validations for order not available po

--- src/components/P2 system/ReleaseSchedulePoModal.js
+++ src/components/P2 system/ReleaseSchedulePoModal.js
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 import { Button, Form, Modal, Table } from "react-bootstrap";
 import { useDispatch } from "react-redux";
 import { OrderNotAvailableDataPo } from "../../redux/actions/P2SystemActions";
+import Swal from "sweetalert2";
 
 const ReleaseSchedulePoModal = ({
   handleCloseReleaseSchedulePoModal,
@@ -10,68 +11,52 @@ const ReleaseSchedulePoModal = ({
   OrderNotAvailable,
 }) => {
   const [dateFilter, setDateFilter] = useState("");
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
   const handleDateChange = (e) => {
     setDateFilter(e.target.value);
   };
 
-  // const reminderDatee =
-  // const handleFinishButtonClick = async () => {
-  //   try {
-  //     const { user_id } = userData ?? {};
+  const handleSubmitReleaseSchedulePoModal = async () => {
+    const poId = OrderNotAvailable.map((order) => order.po_id);
+    const orderId = OrderNotAvailable.map((order) => order.order_id);
+    const productId = OrderNotAvailable.map((order) => order.product_id);
+    const productQuantity = OrderNotAvailable.map((order) => order.quantity);
+    const customerStatus = OrderNotAvailable.map(
+      (order) => order.customer_status
+    );
+    const reminder = dateFilter;
 
-  //     await dispatch(CustomOrderFinish({ user_id, id }))
-  //       .then((response) => {
-  //         if (response.data.status == "Completed") {
-  //           Swal.fire({
-  //             text: response.data?.message,
-  //           }).then((result) => {
-  //             if (result.isConfirmed) {
-  //               navigate("/ordersystem");
-  //             }
-  //           });
-  //         }
-  //         if (response.data.status == "Dispatch Image") {
-  //           Swal.fire({
-  //             text: response.data?.message,
-  //           });
-  //         }
-  //         if (response.data.status == "P2") {
-  //           Swal.fire({
-  //             text: response.data?.message,
-  //           });
-  //         }
-  //       })
-  //       .catch((error) => {
-  //         console.error(error);
-  //       });
-  //   } catch (error) {
-  //     console.error("Error while attaching file:", error);
-  //   }
-  // };
+    const requestData = {
+      po_id: poId,
+      order_id: orderId,
+      product_id: productId,
+      quantity: productQuantity,
+      customer_status: customerStatus,
+      reminder_date: reminder,
+    };
 
-  const handleSubmitReleaseSchedulePoModal=async()=>{
-    try {
-    const poId = OrderNotAvailable.map((order) =>order.po_id)
-  const orderId = OrderNotAvailable.map(order => order.order_id);
-  const productId =  OrderNotAvailable.map(order => order.product_id);
-  const productQuantity =  OrderNotAvailable.map(order => order.quantity);
-  const customerStatus = OrderNotAvailable.map(order => order.customer_status);
-  const reminder = dateFilter;
-  await dispatch(OrderNotAvailableDataPo({
-    po_id:poId,
-    order_id:orderId,
-    product_id:productId,
-    quantity:productQuantity,
-    customer_status:customerStatus,
-    reminder_date:reminder
-  }))
-    }catch{
+    await dispatch(OrderNotAvailableDataPo(requestData))
+      .then((response) => {
+        if (response.data.message === "Sales Orders created successfully") {
+          Swal.fire({
+            icon: "success",
+            title: "Uploaded Successfully!",
+          });
+        }
+        handleCloseReleaseSchedulePoModal();
+        OrderNotAvailable([]);
+      })
+      .then((response) => {
+        handleCloseReleaseSchedulePoModal();
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
 
-    }
+  console.log(OrderNotAvailable,'OrderNotAvailable from modal')
 
-  }
   return (
     <div>
       <Modal show={showModal} onHide={handleCloseReleaseSchedulePoModal}>
