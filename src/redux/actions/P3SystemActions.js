275f3cd841972100a900e01f3dcd86b3842f35df
codecraftwt <codecraftwt@gmail.com>
2024-05-20 20:30:19 +0530

    send for prep

--- src/redux/actions/P3SystemActions.js
+++ src/redux/actions/P3SystemActions.js
@@ -1,5 +1,9 @@
 import axios from "axios";
 import {
+  ADD_PRODUCT_ORDER_FOR_PREP_FAIL,
+  ADD_PRODUCT_ORDER_FOR_PREP_REQUEST,
+  ADD_PRODUCT_ORDER_FOR_PREP_SUCCESS,
+  API_URL,
   GET_PRODUCT_MANUAL_FAIL,
   GET_PRODUCT_MANUAL_REQUEST,
   GET_PRODUCT_MANUAL_SUCCESS,
@@ -38,3 +42,43 @@ export const GetProductOrderDetails =
       dispatch({ type: GET_PRODUCT_ORDER_DETAILS_FAIL, error: error.message });
     }
   };
+
+export const AddProductOrderForPre = (requestedDataP) => async (dispatch) => {
+  try {
+    dispatch({ type: ADD_PRODUCT_ORDER_FOR_PREP_REQUEST });
+    const response = await axios.post(
+      `${API_URL}wp-json/product-instock-api/v1/product-instock-from-factory/`,requestedDataP
+    );
+    dispatch({
+      type: ADD_PRODUCT_ORDER_FOR_PREP_SUCCESS,
+      payload: response?.data,
+    });
+    return response;
+  } catch (error) {
+    dispatch({ type: ADD_PRODUCT_ORDER_FOR_PREP_FAIL, error: error.message });
+  }
+};
+
+// export const OrderNotAvailableDataPo = (requestData) => async (dispatch) => {
+//   try {
+//     if (!requestData?.reminder_date) {
+//       Swal.fire({
+//         icon: "error",
+//         title: "Please select a reminder date!",
+//       });
+//     } else {
+//       dispatch({ type: ADD_ORDER_NOT_AVAILABLE_REQUEST });
+//       const response = await axios.post(
+//         `${API_URL}wp-json/custom-so-create/v1/convert-so-order/`,
+//         requestData
+//       );
+//       dispatch({
+//         type: ADD_ORDER_NOT_AVAILABLE_SUCCESS,
+//         payload: response?.data,
+//       });
+//       return response;
+//     }
+//   } catch (error) {
+//     dispatch({ type: ADD_ORDER_NOT_AVAILABLE_FAIL, error: error.message });
+//   }
+// };
