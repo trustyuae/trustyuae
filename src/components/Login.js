13a2412f11cb250f95b6e22ee09be39bb3f8fbd5
codecraftwt <codecraftwt@gmail.com>
2024-07-09 20:09:48 +0530

    solved disabled button issue

--- src/components/Login.js
+++ src/components/Login.js
@@ -8,11 +8,11 @@ import {
   MDBCardImage,
 } from "mdb-react-ui-kit";
 import Button from "react-bootstrap/Button";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { loginUser, loginUserWithToken } from "../redux/actions/UserActions";
 import { useNavigate } from "react-router-dom";
 import { isValidEmail } from "../utils/validation";
-import styled from 'styled-components';
+import styled from "styled-components";
 import { Box, CardContent } from "@mui/material";
 import { Card, Col, Row } from "react-bootstrap";
 
@@ -57,40 +57,49 @@ const Login = () => {
     }
   };
 
-
   useEffect(() => {
-    const token = localStorage.getItem('token');
+    const token = localStorage.getItem("token");
     dispatch(loginUserWithToken(navigate, token));
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
+  const loading = useSelector((state) => state.loginUser.loading);
+
   return (
-    <MDBContainer style={{ height: '100vh' }} className="p-3">
+    <MDBContainer style={{ height: "100vh" }} className="p-3">
       <Row className="d-flex justify-content-center align-items-center h-100">
         <Col md={10}>
           <Card className="mb-0 px-3">
             <CardContent>
               <Row className="justify-content-center align-items-center">
                 <Col md={6}>
-                  <Box sx={{ height: '350px', width: '100%' }}>
+                  <Box sx={{ height: "350px", width: "100%" }}>
                     {/* <img
                       src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.svg"
                       className="h-100 w-100"
                       alt="Phone"
                     /> */}
                     <img
-                      src={require('../assets/access-control-system.webp')}
+                      src={require("../assets/access-control-system.webp")}
                       className="h-100 w-100"
-                      style={{ objectFit: 'cover' }}
+                      style={{ objectFit: "cover" }}
                       alt="Phone"
                     />
                   </Box>
                 </Col>
                 <Col md={5}>
-                  <Box className="mb-3" sx={{
-                    width: '150px'
-                  }}>
-                    <img className="h-100 w-100" style={{ objectFit: 'contain' }} src={require('../assets/logo-large-main.webp')} alt="logo" />
+                  <Box
+                    className="mb-3"
+                    sx={{
+                      width: "150px",
+                    }}
+                  >
+                    <img
+                      className="h-100 w-100"
+                      style={{ objectFit: "contain" }}
+                      src={require("../assets/logo-large-main.webp")}
+                      alt="logo"
+                    />
                   </Box>
                   <MDBInput
                     // wrapperClass="mb-4"
@@ -123,14 +132,26 @@ const Login = () => {
                       labelClass="custom-label-style1"
                     />
                   </div>
-                  <Button
-                    className="w-100"
-                    variant="primary"
-                    type="submit"
-                    onClick={handleSubmit}
-                  >
-                    <strong>Sign In</strong>
-                  </Button>
+                  {loading ? (
+                    <Button
+                      className="w-100"
+                      variant="primary"
+                      type="submit"
+                      onClick={handleSubmit}
+                      disabled
+                    >
+                      <strong>{loading ? "Signing In..." : "Sign In"}</strong>
+                    </Button>
+                  ) : (
+                    <Button
+                      className="w-100"
+                      variant="primary"
+                      type="submit"
+                      onClick={handleSubmit}
+                    >
+                      <strong>{"Sign In"}</strong>
+                    </Button>
+                  )}
                 </Col>
               </Row>
             </CardContent>
@@ -143,9 +164,8 @@ const Login = () => {
 
 export default Login;
 
-
 const Error = styled.p`
-  font-size:16px;
+  font-size: 16px;
   color: red;
-  fontWeight:400;
+  fontweight: 400;
 `;
