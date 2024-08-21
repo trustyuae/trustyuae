eabd142b43b7e240973bc86cb4719bff48d1cbc4
codecraftwt <codecraftwt@gmail.com>
2024-08-21 14:45:16 +0530

    deleted unwanted code

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Login from "./components/Login";
-// import Dashboard from "./components/Dashboardd";
 import { Layout } from "./routes/Layout";
 import OrderSystem from "./components/P1 system/OrderSystem";
 import FactoryForm from "./components/factory Management/FactoryForm";
 import OrderManagementSystem from "./components/P2 system/OrderManagementSystem";
 import PoDetails from "./components/P2 system/PoDetails";
-import ProtectedRoute from "./utils/ProtectedRoute";
 import AllProductList from "./components/Product management/AllProductList";
 import OrderNotAvailable from "./components/P2 system/OrderNotAvailable";
 import ImageUpload from "./components/ImageUpload";
@@ -19,10 +17,6 @@ import OnHoldManagement from "./components/P3 system/OnHoldManagement";
 import OnHoldManegementSystem from "./components/P3 system/OnHoldManegementSystem";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import OperationAssistantRoute from "./utils/navigation/OperationAssistantRoute";
-import FactoryCoordinatorRoute from "./utils/navigation/FactoryCoordinatorRoute";
-import PageNotFound from "./components/pageNotFound/PageNotFound";
-import CustomerSupportRoute from "./utils/navigation/CustomerSupportRoute";
 import "./index.css";
 import GRNView from "./components/P3 system/GRNView";
 import ExchangeAndReturn from "./components/ER system/ExchangeAndReturn";
@@ -52,15 +46,12 @@ function App() {
         <Routes>
           <Route exact path="/" element={<Login />} />
           <Route element={<Layout />}>
-            {/* <Route element={<ProtectedRoute />}> */}
-            {/* <Route element={<OperationAssistantRoute />}> */}
             <Route exact path="/ordersystem" element={<OrderSystem />} />
-            {/* </Route> */}
             <Route exact path="/factory_form" element={<FactoryForm />} />
             <Route
               exact
               path="/order_management_system"
-              element={<OrderManagementSystem />}
+              element={<OrderManagementSystem />} 
             />
             <Route exact path="/PO_details/:id" element={<PoDetails />} />
             <Route
@@ -68,23 +59,22 @@ function App() {
               path="/all_products_list"
               element={<AllProductList />}
             />
-            {/* <Route element={<CustomerSupportRoute />}> */}
+
             <Route
               exact
               path="/order_not_available"
               element={<OrderNotAvailable />}
             />
-            {/* </Route> */}
+
             <Route exact path="/image_upload" element={<ImageUpload />} />
             <Route exact path="/order_details/:id" element={<OrderDetails />} />
             <Route exact path="/all_factory" element={<AllFactory />} />
-            {/* <Route element={<FactoryCoordinatorRoute />}> */}
+
             <Route
               exact
               path="/PO_ManagementSystem"
               element={<POManagementSystem />}
             />
-            {/* </Route> */}
             <Route exact path="/GRN_Management" element={<GRNManagement />} />
             <Route exact path="/GRN_View/:id" element={<GRNView />} />
             <Route
@@ -181,12 +171,17 @@ function App() {
               element={<OnHoldOrdersdetailsInChina />}
             />
 
-            <Route exact path="/missing_orders_system" element={<MissingOrderSystem />} />
-            <Route exact path="/missing_order_details/:id" element={<MissingOrderDetails />} />
-
-            <Route exact path="/PageNotFound" element={<PageNotFound />} />
+            <Route
+              exact
+              path="/missing_orders_system"
+              element={<MissingOrderSystem />}
+            />
+            <Route
+              exact
+              path="/missing_order_details/:id"
+              element={<MissingOrderDetails />}
+            />
           </Route>
-          {/* </Route> */}
         </Routes>
         <ToastContainer />
       </BrowserRouter>
--- src/components/Dashboardd.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Dashboardd = () => {
-  return (
-    <div>Dashboardd</div>
-  )
-}
-
-export default Dashboardd
