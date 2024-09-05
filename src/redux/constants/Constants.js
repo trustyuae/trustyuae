e9c1d12bf7554408c095d42170638a6a3b14cbd4
codecraftwt <codecraftwt@gmail.com>
2024-09-05 17:52:23 +0530

    added changes regarding base url changes

--- src/redux/constants/Constants.js
+++ src/redux/constants/Constants.js
@@ -1,9 +1,9 @@
-// export const url = "https://trustyuae.com/wp-json/";
-// export const API_URL = "https://trustyuae.com/";
+export const url = "https://trustyuae.com/wp-json/";
+export const API_URL = "https://trustyuae.com/";
 
-export const url =
-  "https://ghostwhite-guanaco-836757.hostingersite.com/wp-json/";
-export const API_URL = "https://ghostwhite-guanaco-836757.hostingersite.com/";
+// export const url =
+//   "https://ghostwhite-guanaco-836757.hostingersite.com/wp-json/";
+// export const API_URL = "https://ghostwhite-guanaco-836757.hostingersite.com/";
 
 // export const API_URL_N = "https://ghostwhite-guanaco-836757.hostingersite.com/"
 
