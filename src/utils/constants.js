184ea74361322d07627cef8861dd33fda273bea7
codecraftwt <codecraftwt@gmail.com>
2024-09-11 19:19:46 +0530

    added changes regarding baseurl changes

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,10 +1,12 @@
-export const loginURL = 'https://wordpress.trustysystem.com//wp-json/custom-login/v1/login'
+export const loginURL =
+  "https://wordpress.trustysystem.com//wp-json/custom-login/v1/login";
 
-export const logoutURL = 'https://wordpress.trustysystem.com//wp-json/custom-login/v1/logout'
+export const logoutURL =
+  "https://wordpress.trustysystem.com//wp-json/custom-login/v1/logout";
 
-// export const url = "https://trustyuae.com/wp-json/";
-// export const API_URL = "https://trustyuae.com/";
+export const url = "https://trustyuae.com/wp-json/";
+export const API_URL = "https://trustyuae.com/";
 
-export const url =
-  "https://ghostwhite-guanaco-836757.hostingersite.com/wp-json/";
-export const API_URL = "https://ghostwhite-guanaco-836757.hostingersite.com/";
+// export const url =
+//   "https://ghostwhite-guanaco-836757.hostingersite.com/wp-json/";
+// export const API_URL = "https://ghostwhite-guanaco-836757.hostingersite.com/";
