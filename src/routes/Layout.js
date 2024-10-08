fe569f3718b8794ed6122c0bf79890a48ff7adb1
codecraftwt <codecraftwt@gmail.com>
2024-10-08 11:21:00 +0530

    added changes regarding table width adjustment

--- src/routes/Layout.js
+++ src/routes/Layout.js
@@ -28,12 +28,13 @@ const StyledMain = styled("main")(({ theme, isSidebarVisible }) => ({
   height: "100%",
   flex: 1,
   overflow: "auto",
-  width: "100%",
   paddingTop: "65px",
   paddingBottom: "6rem",
-  backgroundColor: 'rgb(241, 239, 241)',
-  marginLeft: isSidebarVisible ? "300px" : "0px", // Conditional margin-left based on sidebar visibility
-  transition: "margin-left 0.3s ease", // Add smooth transition for better UX
+  backgroundColor: "rgb(241, 239, 241)",
+  // Use calc for width to handle sidebar visibility
+  width: isSidebarVisible ? "calc(100% - 300px)" : "100%",
+  marginLeft: isSidebarVisible ? "300px" : "0px",
+  transition: "margin-left 0.3s ease, width 0.3s ease", // Add width transition for smooth adjustment
 }));
 
 export const Layout = () => {
@@ -43,9 +44,9 @@ export const Layout = () => {
     function handleResize() {
       setIsSidebarVisible(window.innerWidth > 1024);
     }
-    window.addEventListener('resize', handleResize);
+    window.addEventListener("resize", handleResize);
     handleResize(); // Call the function once to set initial state
-    return () => window.removeEventListener('resize', handleResize); // Clean up event listener
+    return () => window.removeEventListener("resize", handleResize); // Clean up event listener
   }, []);
 
   const toggleSidebar = () => {
@@ -58,7 +59,11 @@ export const Layout = () => {
         <Header onToggleSidebar={toggleSidebar} />
         {isSidebarVisible && <SideBar />}
         {/* Pass isSidebarVisible as a prop to StyledMain */}
-        <StyledMain id="main" isSidebarVisible={isSidebarVisible} className="overflow-visible">
+        <StyledMain
+          id="main"
+          isSidebarVisible={isSidebarVisible}
+          className="overflow-visible"
+        >
           <Card className="border-0 p-2 overflow-visible">
             <Outlet />
           </Card>
